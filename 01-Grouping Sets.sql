727bdd8f9bbbf37b78d0ff955c9b458c0c88c61e
Arkadiusz Choczaj <arkadiusz.choczaj@gmail.com>
2017-02-09 22:22:06 +0000

    Update 01-Grouping Sets.sql
    Add comment to one of a GROUP BY GROUPING SETS option to show which one is a GROUPING SETS ROLLUP equivalent

--- 01-Grouping Sets.sql
+++ 01-Grouping Sets.sql
@@ -4,7 +4,7 @@ LEFT JOIN SalesLT.Product AS prd
 ON prd.ProductCategoryID = cat.ProductcategoryID
 --GROUP BY cat.ParentProductCategoryName, cat.ProductCategoryName
 --GROUP BY GROUPING SETS(cat.ParentProductCategoryName, cat.ProductCategoryName,())
---GROUP BY GROUPING SETS(cat.ParentProductCategoryName,(cat.ParentProductCategoryName, cat.ProductCategoryName),())
+--GROUP BY GROUPING SETS(cat.ParentProductCategoryName,(cat.ParentProductCategoryName, cat.ProductCategoryName),()) --GROUPING SETS ROLLUP Equivalent
 GROUP BY ROLLUP (cat.ParentProductCategoryName, cat.ProductCategoryName)
 -- GROUP BY CUBE (cat.ParentProductCategoryName, cat.ProductCategoryName)
 ORDER BY cat.ParentProductCategoryName, cat.ProductCategoryName;
